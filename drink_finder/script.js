ec1c4fb9cb250049060883e88d2b4ef0c36aec5a
Hubert <122702462+HubertWojnowski@users.noreply.github.com>
2024-05-26 14:03:03 +0200

    done

--- drink_finder/script.js
+++ drink_finder/script.js
@@ -7,9 +7,7 @@ const handleInputChange = (e) => {
 
 
     li.forEach(el => {
-        const task = el.textContent
-
-        if (task.toLowerCase().indexOf(text) !== -1) {
+        if (el.textContent.toLowerCase().indexOf(text) !== -1) {
             el.style.display = 'block'
         } else {
             el.style.display = 'none'
