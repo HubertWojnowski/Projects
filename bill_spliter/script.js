8dd76a703debbb2410187e073f79bc2570a3f7c1
Hubert <122702462+HubertWojnowski@users.noreply.github.com>
2024-05-26 23:01:26 +0200

    keep working on js

---
+++ bill_spliter/script.js
@@ -0,0 +1,9 @@
+const bill = document.querySelector('#price')
+const peopleAmount = document.querySelector('#people')
+const tipAmount = document.querySelector('#tip')
+const button = document.querySelector('.count')
+const error = document.querySelector('.error')
+const cost = document.querySelector('.cost')
+
+
+
