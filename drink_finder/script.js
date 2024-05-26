1185ce6e1fa3c2540f792b551095424a3ce4ede8
Hubert <122702462+HubertWojnowski@users.noreply.github.com>
2024-05-26 14:02:17 +0200

    done

--- drink_finder/script.js
+++ drink_finder/script.js
@@ -1,1 +1,20 @@
-console.log('ioasudh');
+const search = document.querySelector('.search')
+const li = document.querySelectorAll('li')
+
+const handleInputChange = (e) => {
+
+    const text = e.target.value.toLowerCase();
+
+
+    li.forEach(el => {
+        const task = el.textContent
+
+        if (task.toLowerCase().indexOf(text) !== -1) {
+            el.style.display = 'block'
+        } else {
+            el.style.display = 'none'
+        }
+    })
+}
+
+search.addEventListener('keyup', handleInputChange)
